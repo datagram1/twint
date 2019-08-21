a1c01827ff9d88290fd5eeb04748e522a6d5f827
Datagram <datagram1@justmyemail.co.uk>
2019-08-21 22:11:05 +0100

    Dedupe System
    added face detect and filerename plus skip

--- twint/dedupe_files.py
+++ twint/dedupe_files.py
@@ -7,6 +7,9 @@ import hashlib
 import logging
 from PIL import Image
 import imagehash
+import face_recognition
+import cv2 as cv
+
 
 logger = logging.getLogger(__name__)
 
@@ -51,27 +54,73 @@ def findDup(parentFolder):
     return dups
 
 def is_image(filename):
-    f = filename.lower()
-    return f.endswith(".png") or f.endswith(".jpg") or \
-       f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif") or '.jpg' in f
+    log = logging.getLogger('is_image')
+    # log.info(f"is_image starting ...")
+    try:
+        f = filename.lower()
+        return f.endswith(".png") or f.endswith(".jpg") or \
+        f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif") or '.jpg' in f
+    except RuntimeError as e:
+        log.critical(f"is_image error: {str(e)}")
 
-def find_similar_images(userpaths):
-    hashfunc = imagehash.average_hash
-
-
-    image_filenames = []
+def get_filenames(userpaths):
+    file_list = []
     for dirName, subdirs, fileList in os.walk(userpaths):
         for filename in fileList:
             if is_image(filename):
-                image_filenames.append(os.path.join(dirName, filename))
-    num = 1
-    images = {}
-    for img in sorted(image_filenames):
-        try:
-            # hash = imagehash.dhash(Image.open(img), 10)
+                file_list.append(os.path.join(dirName, filename))
+    return file_list
+
+def face_detect_fast(img):
+    image = face_recognition.load_image_file(img)
+    face_landmarks_list = face_recognition.face_landmarks(image)
+
+    faces_detected = len(face_landmarks_list)
+    if faces_detected:
+        return True
+    else:
+        return False
+
+def face_detect_better(img):
+
+    dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'twint/profiles'))
+    face_cascade_list = [
+        f'{dir_path}/haarcascade_frontalface_alt2.xml',
+        f'{dir_path}/haarcascade_profileface.xml',
+        f'{dir_path}/haarcascade_frontalface_default.xml',
+        f'{dir_path}/haarcascade_fullbody.xml']
+
+    image = cv.imread(img)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    for fc in face_cascade_list:
+
+        face_cascade = cv.CascadeClassifier(fc)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces_detected = len(faces)
+
+        if faces_detected:
+            return True
+    return False
+
+
+def find_similar_images(userpaths):
+    hashfunc = imagehash.average_hash
+    if not os.path.exists(userpaths):
+        Print(f"Path doesn't exist: {userpaths}")
+        exit(1)
+    try:
+        image_filenames = get_filenames(userpaths)
+
+        num = 1
+        images = {}
+        for img in sorted(image_filenames):
+            if not os.path.isfile(img):
+                print("file not found")
+                return
+            hash = imagehash.dhash(Image.open(img), 10)
             # hash = imagehash.average_hash(Image.open(img), 16)
             # phash lower number matches everything to everything
-            hash = imagehash.phash(Image.open(img), 4, 12)
+            # hash = imagehash.phash(Image.open(img), 4, 12)
 
             # hash = imagehash.whash(Image.open(img), 4)
             # hash = imagehash.whash(Image.open(img), mode='db4')
@@ -82,34 +131,55 @@ def find_similar_images(userpaths):
             new_name = os.path.join(path, new_nameSTR)
 
             # print(f"hash: {hash}  \t image: {img}")
-        except Exception as e:
-            print('Problem:', e, 'with', img)
-        if hash in images:
-            # print(f"len hash: {len(hash)}")
-            # new_name = f"d{hash}-{images}{os.path.splitext(img)[1]}"
-            # print(f"\r moop {img} \t already exists as ".join(images[hash]))
-            # print("-----------")
-            # print(f"{img}")
-            # print("-----------")
-            if 'dupPictures' in img:
-                print('rm -v', img)
-        # os.rename(img, new_name)
-        # simple progress bar
 
-        # if new_name:
-            # print(f"{new_name} \t\t oldname {img}")
-        images[hash] = images.get(hash, []) + [img]
-        num = num + 1
+            if hash in images:
+                # its a duplicate
+                if 'dupPictures' in img:
+                    print('rm -v', img)
+            # os.rename(img, new_name)
+            # images[hash] = images.get(hash, []) + [img]
+
+            filename = os.path.split(img)[1]
+            if filename[:2] == "F-":
+                print(f"{img}  --  Skipped file")
+                continue
+            else:
+                # faces_detected = face_detect_fast(img)
+                faces_detected = face_detect_better(img)
+
+            if faces_detected:
+                new_nameSTR_face = f"F-{hash}-{num}{os.path.splitext(img)[1]}"
+                new_name = os.path.join(path, new_nameSTR_face)
+
+                print(f"{new_name} \t faces detected")
+
+                images[hash] = images.get(hash, []) + [new_name]
+                try:
+                    os.rename(img, new_name)
+                except OSError:
+                    ext = filename[4:]
+                    # need to add a b before last 3 characters here
+                    # os.rename(img, new_name + "b")
+                    print("test renaming file: " + os.path.join(path, filename + "b" + ext))
+            else:
+                images[hash] = images.get(hash, []) + [new_name]
+                os.rename(img, new_name)
+                print(f"{new_name}")
+
+            num = num + 1
+    except Exception as e:
+            print('Problem:', e, 'with', img)
+
 
     # for k, img_list in six.iteritems(images):
     #    if len(img_list) > 1:
     #        print(" ".join(img_list))
     printResults(images)
 
-def progress_bar(counter, range)
+def progress_bar(counter, range):
     for j in trange(66, leave=True):
-#    sleep(0.1)
-    pass
+    #    sleep(0.1)
+        pass
 
 
 # Joins two dictionaries
@@ -175,7 +245,7 @@ def printResults(dict1):
             print(f"Keeping file: {shortest}")
             for file_to_delete in result:
                 if not file_to_delete == shortest:
-                    # os.remove(file_to_delete)
+                    os.remove(file_to_delete)
                     print(f"Deleted: {file_to_delete}")
 
             print('--------------------')
